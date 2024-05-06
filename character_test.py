c164230f7a3716c9b18c4595702795ef237bd1fc
LuOhana <141501914+LuOhana@users.noreply.github.com>
2024-05-06 18:17:56 +0100

    Add files via upload

---
+++ character_test.py
@@ -0,0 +1,9 @@
+from character import Character
+dave = Character("Dave", "A smelly zombie")
+
+dave.describe()
+
+#add a line of dialogue to Dave
+dave.set_conversation("Hello there! I am going to join your OOP game soon!")
+
+dave.talk()
